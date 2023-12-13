7239a0fdd03a4e09c8e785e0e0dded9c97f7ee80
fer-gab-sua <fer.gab.sua@gmail.com>
2023-12-13 16:42:03 -0300

    Update Creacion de Tablas.sql

--- Query SQL/Creacion de Tablas.sql
+++ Query SQL/Creacion de Tablas.sql
@@ -7,6 +7,8 @@ END;
 -- Usar la base de datos
 USE GuardiasMedicas;
 
+
+-----------------------------------------------------------------------------------------------------------------------
 -- Comprobar si la tabla existe y, si no, crearla
 IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Usuarios')
 BEGIN
@@ -18,6 +20,7 @@ BEGIN
     );
 END;
 
+-----------------------------------------------------------------------------------------------------------------------
 -- Comprobar si la tabla existe y, si no, crearla
 IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Permisos')
 BEGIN
@@ -29,5 +32,70 @@ BEGIN
 		
     );
 END;
+-----------------------------------------------------------------------------------------------------------------------
 
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Guardias')
+BEGIN
+	--Creo la tabla principal
+	CREATE TABLE Guardias (
+	grd_int_id INT PRIMARY KEY,
+	grd_int_idmovil INT,
+	grd_int_idparamedico INT,
+	grd_int_idmedico INT,
+	grd_int_idenfermero INT,
+	grd_fyh_inicio DATE,
+	grd_fyh_fin DATE,
+	grd_int_duracionguardiahs INT,
+	grd_txt_observaciones NVARCHAR(120),
+	grd_txt_estado NVARCHAR(20)
+	);
+END;
+-----------------------------------------------------------------------------------------------------------------------
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Moviles')
+BEGIN
+	--Creo la tabla principal
+	CREATE TABLE Moviles(
+	mov_int_id INT PRIMARY KEY,
+	mov_txt_movil NVARCHAR(10),
+	mov_txt_patente NVARCHAR(10)
+	);
+END;
+
+-----------------------------------------------------------------------------------------------------------------------
+
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Paramedicos')
+BEGIN
+	--Creo la tabla principal
+	CREATE TABLE Paramedicos (
+	par_int_id INT PRIMARY KEY,
+	par_int_legajo INT,
+	par_txt_nombre NVARCHAR(30),
+	par_txt_apellido NVARCHAR(30),
+	par_fch_vencimientolicencia DATE
+	);
+END;
+
+-----------------------------------------------------------------------------------------------------------------------
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Medicos')
+BEGIN
+	--Creo la tabla principal
+	CREATE TABLE Medicos (
+	med_int_id INT PRIMARY KEY,
+	med_txt_nombre NVARCHAR(30),
+	med_txt_apellido NVARCHAR(30),
+	med_int_matricula INT,
+	);
+END;
+
+-----------------------------------------------------------------------------------------------------------------------
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Enfermeros')
+BEGIN
+	--Creo la tabla principal
+	CREATE TABLE Enfermeros(
+	enf_int_id INT PRIMARY KEY,
+	enf_txt_nombre NVARCHAR(30),
+	enf_txt_apellido NVARCHAR(30),
+	enf_int_matricula INT,
+	);
+END;
 
