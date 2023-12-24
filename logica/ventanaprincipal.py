770f851efb6fca0dd939826248d17d08f1d6265c
fer-gab-sua <fer.gab.sua@gmail.com>
2023-12-24 19:46:57 -0300

    v1.0

--- logica/ventanaprincipal.py
+++ logica/ventanaprincipal.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets, uic 
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit,QPushButton
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit,QPushButton ,QMessageBox
 
 from modelo.Conect import ConsultasSql , AltaGuardia
 
@@ -20,26 +20,27 @@ class Ui_VentanaPrincipal(QtWidgets.QMainWindow):
         self.actionCabina.triggered.connect(lambda: self.StackedWidget_stwid_1.setCurrentIndex(3))
         self.actionEnfermeros.triggered.connect(lambda: self.StackedWidget_stwid_1.setCurrentIndex(4))
         self.actionHistorial.triggered.connect(lambda: self.StackedWidget_stwid_1.setCurrentIndex(5))
-        #conecto todo lo referido a la primera pantalla 1 - ALTA DE GUARDIA
+        #conecto todo lo referido a la primera pantalla 1 - ALTA DE GUARDIA - BASES
         
-        # Configuración del QTableView
-        header_labels = ['Base', 'Patente']
-        
-        # Crear el modelo de ítems
-        self.model = QStandardItemModel()
-        self.model.setHorizontalHeaderLabels(header_labels)
-
+        # Crear el modelo de ítems para la base
+        self.model_moviles = QStandardItemModel()
         self.fill_adg_tblv_1()
         # Asignar el modelo a la QTableView
-        self.adg_tblv_1.setModel(self.model)
-        #lleno el combo box
-        self.fill_adg_comb_base()
-        self.adg_comb_base.currentIndexChanged.connect(self.select_adg_comb_base)
-
+        self.adg_tblv_1.setModel(self.model_moviles)
+        #conecto el dobleclick de moviles a la funcion
+        self.adg_tblv_1.doubleClicked.connect(self.select_movil)
         #conecto el add y del de la base de moviles
         self.adg_btn_addbase.clicked.connect(lambda:self.addbase())
         self.adg_btn_delbase.clicked.connect(lambda:self.delbase())
 
+        #conecto todo lo referido a la primera pantalla 1 - ALTA DE GUARDIA - PARAMEDICOS
+        self.model_paramedicos = QStandardItemModel()
+        self.fill_adg_tblv_2()
+        self.adg_tblv_2.setModel(self.model_paramedicos)
+        self.adg_tblv_2.doubleClicked.connect(self.select_paramedico)
+        #conecto el add y del de la base de paramedicos
+        self.adg_btn_addparamedico.clicked.connect(lambda:self.addparamedico())
+        self.adg_btn_delparamedico.clicked.connect(lambda:self.delparamedico())
 
 
     def permisos(self,usuario):
@@ -70,40 +71,127 @@ class Ui_VentanaPrincipal(QtWidgets.QMainWindow):
                     print("permiso 5")
                     self.actionHistorial.setEnabled(True)
 
-    """########## TODO ALTA DE GUARDIAS #######################"""
+    """########## TODO ALTA DE GUARDIAS ####################### BASE"""
     def fill_adg_tblv_1(self,):
         bases_datos = self.sqlaltaguardia.fill_Bases_sql()
+        self.model_moviles.clear()
+        # Configuración del QTableView
+        header_labels = ['Base', 'Patente']
+        self.model_moviles.setHorizontalHeaderLabels(header_labels)
 
         for tupla in bases_datos:
             fila = tupla 
             
             row_items = [QStandardItem(str(dato)) for dato in fila]
-            self.model.appendRow(row_items)
+            self.model_moviles.appendRow(row_items)
 
-    def fill_adg_comb_base(self):
-        bases_datos = self.sqlaltaguardia.fill_Bases_sql()
+    def select_movil(self):
+        indice_seleccionado = self.adg_tblv_1.currentIndex()
+        base = self.model_moviles.item(indice_seleccionado.row(), 0).text()
+        print(base)
 
-        for item in bases_datos:
-            self.adg_comb_base.addItem(item[0])
+        patente = self.sqlaltaguardia.patente(base)
 
-    def select_adg_comb_base(self):
-        selected_text = self.adg_comb_base.currentText()
-        patente = self.sqlaltaguardia.patente(selected_text)
-        self.adg_let_patente.setText(str(patente[0][0]))
+        if patente:
+            # Asegúrate de que la lista patente no esté vacía antes de acceder al índice
+            self.adg_let_patente.setText(str(patente[0][0]))
+            titulo_base = (f"Movil: {base}")
+            self.adg_tlbox_1.setItemText(0,titulo_base)
+            self.adg_let_base.setText(str(base))
+        else:
+            # Si patente está vacío, establece el texto en blanco o maneja la situación según tu lógica
+            self.adg_let_patente.setText("")
+            self.adg_tlbox_1.setItemText(0,"")
 
     def addbase(self,):
-        subventanamov = SubVentanaMovil()
+        subventanamov = SubVentanaAddMovil()
         subventanamov.exec_()
-
+        # Después de que se cierra la subventana, actualiza el combo box y la tabla
+        self.fill_adg_comb_base()
+        self.fill_adg_tblv_1()
 
     def delbase(self):
+        # Obtener el índice de la fila seleccionada
+        indice_seleccionado = self.adg_tblv_1.currentIndex()
+        # Verificar si hay una selección válida
+        if indice_seleccionado.isValid():
+            # Obtener el valor de la primera columna (patente) en la fila seleccionada
+            base = self.model_moviles.item(indice_seleccionado.row(), 0).text()
+            print(base)
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Question)
+            msg.setText(f"¿Estás seguro de eliminar el registro con movil {base}?")
+            msg.setWindowTitle("Confirmación de eliminación")
+            msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+            # Mostrar el cuadro de mensaje y obtener la respuesta del usuario
+            respuesta = msg.exec_()
+            # Procesar la respuesta
+            if respuesta == QMessageBox.Yes:
+                # Si el usuario hizo clic en "Sí", eliminar el registro
+                self.sqlaltaguardia.borrar_movil(base)
+            else:
+                # Si el usuario hizo clic en "No" o cerró la ventana, no hacer nada
+                print("Eliminación cancelada.")
+            self.fill_adg_comb_base()
+            self.fill_adg_tblv_1()
+        else:
+            print("No hay una fila seleccionada")
+            QMessageBox.warning(self, 'Advertencia', 'Por favor, selecciona una fila antes de intentar borrar.',
+                                QMessageBox.Ok)
+
+    """########## TODO ALTA DE GUARDIAS ####################### PARAMEDICO"""
+
+    def fill_adg_tblv_2(self,):
+        
+        # Asignar el modelo a la QTableView
+        self.adg_tblv_2.setModel(self.model_paramedicos)
+
+        bases_datos = self.sqlaltaguardia.fill_paramedicos_sql()
+        # Configuración del QTableView
+        header_labels_paramedicos = ['Legajo', 'Nombre' , 'Apellido' , 'Licencia Venc.'] 
+        # Crear el modelo de ítems
+        self.model_paramedicos.setHorizontalHeaderLabels(header_labels_paramedicos)
+
+        for tupla in bases_datos:
+            fila = tupla 
+            
+            row_items = [QStandardItem(str(dato)) for dato in fila]
+            self.model_paramedicos.appendRow(row_items)
+
+    def select_paramedico(self):
+        indice_seleccionado = self.adg_tblv_2.currentIndex()
+        legajo = self.model_paramedicos.item(indice_seleccionado.row(), 0).text()
+        print(legajo)
+        paramedico = self.sqlaltaguardia.paramedicos_datos(legajo)
+        legajo = paramedico[0][0]
+        nombre_apellido = paramedico[0][1] + " " + paramedico[0][2]
+        fecha = paramedico[0][3]
+        
+        if paramedico:
+            # Asegúrate de que la lista patente no esté vacía antes de acceder al índice
+            self.adg_let_legajoparamedico.setText(str(legajo))
+            titulo_paramedico = (f"Paramedico: {nombre_apellido}")
+            self.adg_let_nombreyapellido.setText(nombre_apellido)
+            self.adg_date_licenciaparamedico.setDate(fecha)
+            self.adg_tlbox_1.setItemText(1,titulo_paramedico)
+        else:
+            # Si patente está vacío, establece el texto en blanco o maneja la situación según tu lógica
+            self.adg_let_legajoparamedico.setText("")
+            self.adg_let_nombreyapellido.setText("")
+            self.adg_date_licenciaparamedico.setDate("")
+            self.adg_tlbox_1.setItemText(1,"Paramedico")
+
+    def addparamedico(self):
         pass
-    
-    
-    
-class SubVentanaMovil(QDialog):
+
+    def delparamedico(self):
+        pass
+
+
+
+class SubVentanaAddMovil(QDialog):
     def __init__(self):
-        super(SubVentanaMovil, self).__init__()
+        super(SubVentanaAddMovil, self).__init__()
 
         # Crear widgets para la subventana
         self.label_movil = QLabel("Movil:")
@@ -132,3 +220,5 @@ class SubVentanaMovil(QDialog):
         print(movil,patente)
         sql = AltaGuardia()
         sql.alta_movil(movil, patente)
+        self.accept()
+
--- modelo/Conect.py
+++ modelo/Conect.py
@@ -1,7 +1,7 @@
 import pyodbc
 
 class ConexionConBase:
-    def __init__(self) -> None:
+    def __init__(self):
         self.server = 'DESKTOP-PP1EQCP\SQLEXPRESS'
         self.database = 'GuardiasMedicas'
         self.username = 'sa'
@@ -26,106 +26,66 @@ class ConexionConBase:
         if self.conn:
             self.conn.close()
             print("Conexión cerrada.")
+            
+    def ejecutar_consulta(self, consulta, *params):
+        try:
+            self.conectar()
+            cursor = self.conn.cursor()
+            cursor.execute(consulta, params)
+
+            # Verificar si la consulta es de tipo SELECT
+            if "SELECT" in consulta.upper():
+                resultado = cursor.fetchall()
+                return resultado
+            else:
+                self.conn.commit()
+                return None
+
+        except pyodbc.Error as ex:
+            print("Error al ejecutar la consulta:", ex)
+            return None
+        finally:
+            self.cerrar_conexion()
 
 class ConsultasSql(ConexionConBase):
-    def validar_usuarios(self,usuario,contraseña):
-        try:
-            self.conectar()
-            cursor = self.conn.cursor()
-            consulta = "Select Usuario,Pass from Usuarios WHERE Usuario = ?"
-            cursor.execute(consulta,usuario)
-            resultado = cursor.fetchall()
-            print(resultado)
-            if usuario == resultado[0][0] and contraseña == resultado[0][1]:
-                return True
-            else:
-                return False
-            
-        except pyodbc.Error as ex:
-            print("Error al ejecutar la consulta:", ex)
-            return None
-        finally:
-            self.cerrar_conexion()
+    def validar_usuarios(self, usuario, contraseña):
+        consulta = "SELECT Usuario, Pass FROM Usuarios WHERE Usuario = ?"
+        resultado = self.ejecutar_consulta(consulta, usuario)
+        if resultado and usuario == resultado[0][0] and contraseña == resultado[0][1]:
+            return True
+        return False
 
-    def trae_permisos(self,usuario):
-        try:
-            self.conectar()
-            cursor = self.conn.cursor()
-            consulta = """
-                        SELECT Usuarios.Usuario, Permisos.VentanaHabilitada
-                        FROM Permisos
-                        JOIN Usuarios ON Usuarios.Id = Permisos.idUsuario
-                        WHERE Usuarios.Usuario = ?;
-                    """
-            cursor.execute(consulta,usuario)
-            resultado = cursor.fetchall()
-            return resultado
-            
-        except pyodbc.Error as ex:
-            print("Error al ejecutar la consulta:", ex)
-            return None
-        finally:
-            self.cerrar_conexion()
-
+    def trae_permisos(self, usuario):
+        consulta = """
+            SELECT Usuarios.Usuario, Permisos.VentanaHabilitada
+            FROM Permisos
+            JOIN Usuarios ON Usuarios.Id = Permisos.idUsuario
+            WHERE Usuarios.Usuario = ?;
+        """
+        return self.ejecutar_consulta(consulta, usuario)
 
 class AltaGuardia(ConexionConBase):
     def fill_Bases_sql(self):
-        try:
-            self.conectar()
-            cursor = self.conn.cursor()
-            consulta = """
-                        SELECT mov_txt_movil, mov_txt_patente
-                        FROM Moviles
-                    """
-            cursor.execute(consulta)
-            resultado = cursor.fetchall()
-            return resultado
-            
-        except pyodbc.Error as ex:
-            print("Error al ejecutar la consulta:", ex)
-            return None
-        finally:
-            self.cerrar_conexion()
+        consulta = "SELECT mov_txt_movil, mov_txt_patente FROM Moviles"
+        return self.ejecutar_consulta(consulta)
 
-    def patente(self,base):
-        try:
-            self.conectar()
-            cursor = self.conn.cursor()
-            consulta = """
-                        SELECT mov_txt_patente
-                        FROM Moviles
-                        WHERE mov_txt_movil = (?)
-                    """
-            cursor.execute(consulta,base)
-            resultado = cursor.fetchall()
-            return resultado
-            
-        except pyodbc.Error as ex:
-            print("Error al ejecutar la consulta:", ex)
-            return None
-        finally:
-            self.cerrar_conexion()
+    def patente(self, base):
+        consulta = "SELECT mov_txt_patente FROM Moviles WHERE mov_txt_movil = ?"
+        return self.ejecutar_consulta(consulta, base)
 
-    def alta_movil(self,movil,patente):
-        print(movil,patente)
-        try:
-            self.conectar()
-            cursor = self.conn.cursor()
-            consulta = """
-                        SELECT mov_txt_patente
-                        FROM Moviles
-                        WHERE mov_txt_movil = (?)
-                    """
-            cursor.execute(consulta,)
-            resultado = cursor.fetchall()
-            return resultado
-            
-        except pyodbc.Error as ex:
-            print("Error al ejecutar la consulta:", ex)
-            return None
-        finally:
-            self.cerrar_conexion()
-if __name__ == "__main__":
-    consultasql = ConsultasSql()
-    resultado = consultasql.trae_permisos("coordinacionMoviles")
-    print(resultado)
+    def alta_movil(self, movil, patente):
+        consulta = "INSERT INTO [GuardiasMedicas].[dbo].[Moviles] ([mov_txt_movil], [mov_txt_patente]) VALUES (?, ?)"
+        self.ejecutar_consulta(consulta, movil, patente)
+
+    def borrar_movil(self,movil):
+        consulta = "DELETE FROM [GuardiasMedicas].[dbo].[Moviles] WHERE mov_txt_movil = (?)"
+        self.ejecutar_consulta(consulta, movil)
+
+    def fill_paramedicos_sql(self):
+        consulta = "SELECT par_int_legajo, par_txt_nombre, par_txt_apellido, par_fch_vencimientolicencia FROM Paramedicos"
+        return self.ejecutar_consulta(consulta)
+
+    def paramedicos_datos(self, legajo):
+        consulta = "SELECT par_int_legajo, par_txt_nombre, par_txt_apellido, par_fch_vencimientolicencia FROM Paramedicos WHERE par_int_legajo = (?)"
+        return self.ejecutar_consulta(consulta, legajo)
+
